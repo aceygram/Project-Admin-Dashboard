662906331f11513dc290f9c5527f84f0a5cba5dc
Promise Okeke <ace4facebook@gmail.com>
2023-03-18 18:02:25 +0100

    started creating functions for mobile view menu

---
+++ main.js
@@ -0,0 +1,15 @@
+const sidebar = document.querySelector('.sidebar');
+const header = document.querySelector('.header');
+const mainContent = document.querySelector('.main-content');
+const container = document.querySelector('.container');
+
+
+
+
+function showMenu() {
+    sidebar.style.width;
+}
+
+function hideMenu() {
+    navLinks.style.right = "-375px";
+}
